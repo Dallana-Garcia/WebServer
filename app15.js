35fc02e790f62995e121d0182af6cdf1aff236bd
Dallana-Garcia <sgarciay2@miumg.edu.gt>
2023-10-13 10:57:56 -0600

    Segundo Commit

--- app15.js
+++ app15.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('hbs')
 require('dotenv').config();
 
+
 const app = express()
 const port = process.env.PORT
 
@@ -9,6 +10,7 @@ app.set('view engine', 'hbs')
 
 hbs.registerPartials(__dirname + '/views/partials')
 
+app.use(express.static('./public3/template'))
 app.use(express.static('public3'))
 
 app.get('/', (req, res) => {
